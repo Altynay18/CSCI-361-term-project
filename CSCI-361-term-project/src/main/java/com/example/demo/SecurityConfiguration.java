74c896b81df44a94116fc6528d912f2432bb7640
gulnaz-zh <zhambylova25@gmail.com>
2020-11-20 18:55:59 +0600

    Security случайно пушнула свою версию

--- CSCI-361-term-project/src/main/java/com/example/demo/SecurityConfiguration.java
+++ CSCI-361-term-project/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -119,7 +119,7 @@ public class SecurityConfiguration /*extends WebSecurityConfigurerAdapter*/ {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
 
-            http.authorizeRequests()
+            http.antMatcher("/login").authorizeRequests()
                     // URLs matching for access rights
                     .antMatchers("/").permitAll()
                     .antMatchers("/login").permitAll()
